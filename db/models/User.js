0c81b141b5fa4f8e15b3b109e06bed3c63f42515
Olivia Park <olivia.park1346@gmail.com>
2020-08-17 20:19:57 +0900

    added db/models
    course and user models

---
+++ db/models/User.js
@@ -0,0 +1,33 @@
+'use strict';
+const Sequelize = require('sequelize');
+
+module.exports = (sequelize) => {
+  class User extends Sequelize.Model {}
+  User.init({
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    firstName: {
+      type: Sequelize.STRING
+    },
+    lastName: {
+      type: Sequelize.STRING
+    },
+    emailAddress: {
+      type: Sequelize.STRING
+    },
+    password: {
+      type: Sequelize.STRING
+    },
+  }, { sequelize });
+
+  User.associate = (models) => {
+    User.hasMany(models.Courses, {
+
+    });
+  };
+
+  return User;
+}
---
+++ routes.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const express = require('express');
+const { check, validationResult } = require('express-validator/check');
+
+module.exports = router;
+
